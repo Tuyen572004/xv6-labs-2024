3840ae4ae96332dd6a10db8fa071dc6edf567592
Tran Manh Hung <manhhung211.hcmus@gmail.com>
2024-10-16 21:42:32 +0700

    primes commit

--- user/primes.c
+++ user/primes.c
@@ -1,59 +1,67 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-void sieve(int p_left[2]) {
-    int prime, num;
-
-    close(p_left[1]);
-
-    if (read(p_left[0], &prime, sizeof(prime)) == 0) {
-        close(p_left[0]);
-        exit(0); 
-    }
-
-    printf("prime %d\n", prime);
-
-    int p_right[2];
-    pipe(p_right);
-
-    if (fork() == 0) {
-
-        close(p_left[0]);
-        sieve(p_right);
-    } else {
-
-        close(p_right[0]); 
-
-        while (read(p_left[0], &num, sizeof(num)) > 0) {
-            if (num % prime != 0) {
-                write(p_right[1], &num, sizeof(num));
-            }
-        }
-
-        close(p_left[0]);
-        close(p_right[1]);
-        wait(0); 
-        exit(0); 
-    }
+void
+close_pipe(int *p) {
+  close(p[0]);
+  close(p[1]);
 }
 
-int main() {
+void
+primes() {
+  int n, p, len;
+  int fd[2];
 
-    int p_initial[2];
-    pipe(p_initial);
-
-    if (fork() == 0) {
-        sieve(p_initial);
-    } else {
-
-        close(p_initial[0]); 
-
-        for (int i = 2; i <= 280; i++) {
-            write(p_initial[1], &i, sizeof(i));
-        }
-
-        close(p_initial[1]);  
-        wait(0);    
-        exit(0);
+  // read from prev progress 
+  if ((len = read(0, &n, sizeof(int))) <= 0 || n <= 0) {
+    close(1);
+    exit(0);
+  }
+  // write first prime to console
+  printf("prime %d\n", n);
+  
+  pipe(fd);
+  if (fork() == 0) {
+    close(0);
+    dup(fd[0]);
+    close_pipe(fd);
+    primes();
+  } else {
+    close(1);
+    dup(fd[1]);
+    close_pipe(fd);
+    while ((len = read(0, &p, sizeof(int))) > 0 && p > 0) {
+      if (p % n != 0) {
+        write(1, &p, sizeof(int));
+      }
     }
+    if (len <= 0 || p <= 0) {
+      close(1);
+      exit(0);
+    }
+  } 
+}
+
+int
+main(void) {
+  int i;
+  int fd[2];
+  
+  pipe(fd);
+  if (fork() == 0) {
+    close(0);
+    dup(fd[0]);
+    close_pipe(fd);
+    primes();
+  } else {
+    close(1);
+    dup(fd[1]);
+    close_pipe(fd);
+    for (i = 2; i <= 35; i++) {
+      write(1, &i, sizeof(int));
+    }
+    close(1);
+    wait(0);
+  }
+  exit(0);
 }
