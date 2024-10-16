f95697bd4bef2bbf2ad47afa59cf97cd744a54a2
Tran Manh Hung <manhhung211.hcmus@gmail.com>
2024-10-16 16:40:31 +0700

    primes commit

--- user/primes.c
+++ user/primes.c
@@ -1,40 +1,59 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(void) {
-    int c2p[2], p2c[2];
-    pipe(c2p);
-    pipe(p2c);
-    int pid = fork();
-    char buf;
-    char msg = 'a';
+void sieve(int p_left[2]) {
+    int prime, num;
 
-    if (pid == 0) {
-        close(c2p[0]);
-        close(p2c[1]);
+    close(p_left[1]);
 
-        read(p2c[0], &buf, 1);
-        fprintf(2, "%d received ping\n", getpid());
-        write(c2p[1], &msg, 1);
-
-        close(c2p[1]);
-        close(p2c[0]);
-    }
-    else if (pid > 0) {
-        close(c2p[1]);
-        close(p2c[0]);
-
-        write(p2c[1], &msg, 1);
-        read(c2p[0], &buf, 1);
-        fprintf(2, "%d received pong\n", getpid());
-
-        close(c2p[0]);
-        close(p2c[1]);
-    }
-    else {
-        fprintf(3, "fork failed\n");
-        exit(1);
+    if (read(p_left[0], &prime, sizeof(prime)) == 0) {
+        close(p_left[0]);
+        exit(0); 
     }
 
-    exit(0);
+    printf("prime %d\n", prime);
+
+    int p_right[2];
+    pipe(p_right);
+
+    if (fork() == 0) {
+
+        close(p_left[0]);
+        sieve(p_right);
+    } else {
+
+        close(p_right[0]); 
+
+        while (read(p_left[0], &num, sizeof(num)) > 0) {
+            if (num % prime != 0) {
+                write(p_right[1], &num, sizeof(num));
+            }
+        }
+
+        close(p_left[0]);
+        close(p_right[1]);
+        wait(0); 
+        exit(0); 
+    }
+}
+
+int main() {
+
+    int p_initial[2];
+    pipe(p_initial);
+
+    if (fork() == 0) {
+        sieve(p_initial);
+    } else {
+
+        close(p_initial[0]); 
+
+        for (int i = 2; i <= 280; i++) {
+            write(p_initial[1], &i, sizeof(i));
+        }
+
+        close(p_initial[1]);  
+        wait(0);    
+        exit(0);
+    }
 }
